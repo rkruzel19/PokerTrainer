454c3038f273da2b578ea1a6a2e54d68d6679c31
Chewy <rkruzel576@gmail.com>
2021-07-13 06:17:45 -0400

    1st commit. This is an upgraded version of the IgnitionHandAnalyzer repo. Felt it was easier to start over from scratch to implement the database and split file into server & client side(frontend/backend). before everything was in the same file. later commits will have more info but needed to get what I have so far backed up

---
+++ server/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "pokertrainerdb"
+})
+
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/api/login", (req, res) => {
+
+    const sqlInsert = 'SELECT * FROM user WHERE userName="' + req.body.userName + '" && password="' + req.body.password + '"'
+
+    db.query(sqlInsert, (err, result) => {
+        res.send(result)
+    })
+})
+
+app.post("/api/insert", (req, res) => {
+
+    const userName = req.body.userName
+    const password = req.body.password
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    const sqlInsert = "INSERT INTO user (userName, password, firstName, lastName, email) VALUES (?,?,?,?,?)"
+
+    db.query(sqlInsert, [userName, password, firstName, lastName, email], (err, result) => {
+        console.log(err)
+    })
+})
+
+app.listen(3001, () => {
+    console.log("running on port 3001")
+})
